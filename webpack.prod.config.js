31c58c9e23b8c774103a9a25937e1e921042cd1d
vyvyenne <github@beingincognito.co.uk>
2021-08-17 17:56:18 +1000

    update webpack

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -39,11 +39,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          }
-        ]
+        type: 'asset/resource'
       }
     ]
   },
